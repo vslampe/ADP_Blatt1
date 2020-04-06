77f173ac2ca634034b9c5707e1ae3c70e49f257a
vslampe <vslampe52@googlemail.com>
2020-04-06 15:02:33 +0200

    LinkedList von Hari Bufi

---
+++ src/com/company/LinkedListTest.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+
+    @Test
+    public void getAnzahlElemente() {
+    }
+
+    @Test
+    public void insertAt() {
+    }
+
+    @Test
+    public void deleteAt() {
+    }
+
+    @Test
+    public void retrieve() {
+    }
+
+    @Test
+    public void clear() {
+    }
+}
